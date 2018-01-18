c8f5a60ddcdf875bae0bc49f47f2db11ae88ad5f
Pavel <popov.pa@yandex.ru>
2018-01-19 00:55:01 +0300

    webcam detect

--- detect/detector.py
+++ detect/detector.py
@@ -1,16 +1,13 @@
-import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
 import tarfile
-import tensorflow as tf
-import zipfile
-import scipy.misc
 
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
+import numpy as np
+import six.moves.urllib as urllib
+import tensorflow as tf
+# Import everything needed to edit video clips
+import moviepy.editor as mp
+import cv2
 
 if tf.__version__ < '1.4.0':
     raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
@@ -18,7 +15,7 @@ if tf.__version__ < '1.4.0':
 # This is needed to display the images.
 
 # This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("/Projects/image-detector/tf/")
+sys.path.append("/Projects/image-detector/")
 
 from utils import label_map_util
 from utils import visualization_utils as vis_util
@@ -35,13 +32,13 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-        tar_file.extract(file, os.getcwd())
+# opener = urllib.request.URLopener()
+# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+# tar_file = tarfile.open(MODEL_FILE)
+# for file in tar_file.getmembers():
+#    file_name = os.path.basename(file.name)
+#    if 'frozen_inference_graph.pb' in file_name:
+#        tar_file.extract(file, os.getcwd())
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -57,9 +54,9 @@ category_index = label_map_util.create_category_index(categories)
 
 
 def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
+    # (im_width, im_height) = image.size
+    return np.array(image).reshape(
+        (image.shape)).astype(np.uint8)
 
 
 # For the sake of simplicity we will use only 2 images:
@@ -83,12 +80,20 @@ with detection_graph.as_default():
         detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
         detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
         num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-        for image_path in TEST_IMAGE_PATHS:
-            image = Image.open(image_path)
-            # the array based representation of the image will be used later in order to prepare the
-            # result image with boxes and labels on it.
-            image_np = load_image_into_numpy_array(image)
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+
+        # clip = mp.VideoFileClip("test_images/video.mp4").subclip(2, 5).resize(height=180)
+
+        cap = cv2.VideoCapture(0)
+
+        while (True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Our operations on the frame come here
+
+            img_cv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image_np = load_image_into_numpy_array(img_cv)
+
             image_np_expanded = np.expand_dims(image_np, axis=0)
             # Actual detection.
             (boxes, scores, classes, num) = sess.run(
@@ -103,6 +108,12 @@ with detection_graph.as_default():
                 category_index,
                 use_normalized_coordinates=True,
                 line_thickness=2)
-            plt.figure(figsize=IMAGE_SIZE)
-            plt.imshow(image_np)
-            scipy.misc.imsave('outfile.jpg', image_np)
+
+            # Display the resulting frame
+            cv2.imshow('Video', image_np)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
